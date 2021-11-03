29e072f0a6814162acf77982f3e1c242f55c33d5
life4gal <life4gal@gmail.com>
2021-11-03 21:33:21 +0800

    add Doxyfile

--- gal/include/ast_node.hpp
+++ gal/include/ast_node.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #ifndef GAL_LANG_AST_NODE_HPP
-#define GAL_LANG_AST_NODE_HPP
+	#define GAL_LANG_AST_NODE_HPP
 
-#include <string>
-#include <cstdint>
+	#include <cstdint>
+	#include <string>
 
 namespace gal
 {
 	/*! Type of the AST node */
-	enum class ast_type
+	enum class ast_expression_type
 	{
 		// todo: fill it!
-		expression_t,
+		statement_t,
 		integer_t,
 		number_t,
 		string_t,
@@ -23,16 +23,16 @@ namespace gal
 	class ast_expression
 	{
 	public:
-		ast_expression() = default;
+		ast_expression()						  = default;
 
-		virtual ~ast_expression() = 0;
-		ast_expression(const ast_expression&) = default;
-		ast_expression(ast_expression&&) = default;
-		ast_expression& operator=(const ast_expression&) = default;
-		ast_expression& operator=(ast_expression&&) = default;
+		virtual ~ast_expression()				  = 0;
+		ast_expression(const ast_expression&)	  = default;
+		ast_expression(ast_expression&&)		  = default;
+		ast_expression&					  operator=(const ast_expression&) = default;
+		ast_expression&					  operator=(ast_expression&&) = default;
 
 		/*! Returns the type of the node. */
-		constexpr virtual ast_type		  get_type() = 0;
+		constexpr virtual ast_expression_type get_type()				  = 0;
 
 		/*! Returns the textual representation. */
 		[[nodiscard]] virtual std::string to_string() const noexcept { return "ast_expression"; }
@@ -42,7 +42,7 @@ namespace gal
 	class ast_statement : public ast_expression
 	{
 	public:
-		constexpr ast_type		  get_type() override { return ast_type::expression_t; }
+		constexpr ast_expression_type get_type() override { return ast_expression_type::statement_t; }
 		[[nodiscard]] std::string to_string() const noexcept override { return "ast_statement"; }
 	};
 
@@ -58,7 +58,7 @@ namespace gal
 	public:
 		constexpr explicit ast_integer(value_type value) : value_(value) {}
 
-		constexpr ast_type		  get_type() override { return ast_type::integer_t; }
+		constexpr ast_expression_type get_type() override { return ast_expression_type::integer_t; }
 		[[nodiscard]] std::string to_string() const noexcept override;
 	};
 
@@ -74,7 +74,7 @@ namespace gal
 	public:
 		constexpr explicit ast_double(value_type value) : value_(value) {}
 
-		constexpr ast_type		  get_type() override { return ast_type::number_t; }
+		constexpr ast_expression_type get_type() override { return ast_expression_type::number_t; }
 		[[nodiscard]] std::string to_string() const noexcept override;
 	};
 
@@ -90,7 +90,7 @@ namespace gal
 	public:
 		explicit ast_string(value_type value) : value_(std::move(value)) {}
 
-		constexpr ast_type		  get_type() override { return ast_type::string_t; }
+		constexpr ast_expression_type get_type() override { return ast_expression_type::string_t; }
 		[[nodiscard]] std::string to_string() const noexcept override;
 	};
 
@@ -106,9 +106,9 @@ namespace gal
 	public:
 		constexpr explicit ast_boolean(value_type value) : value_(value) {}
 
-		constexpr ast_type		  get_type() override { return ast_type::boolean_t; }
+		constexpr ast_expression_type get_type() override { return ast_expression_type::boolean_t; }
 		[[nodiscard]] std::string to_string() const noexcept override;
 	};
-}
+}// namespace gal
 
 #endif//GAL_LANG_AST_NODE_HPP
